87e35c38cfeb2e59fff3a125814ea2f0f8f85e8f
Diamboy211 <51283163+Diamboy211@users.noreply.github.com>
2021-02-08 20:40:34 +0700

    Update BooleanNum.js

--- BooleanNum.js
+++ BooleanNum.js
@@ -1,1 +1,31 @@
-
+class BooleanNum {
+  constructor(input = 0) {
+    this.a = 0;
+    if (input instanceof Number) {
+      if (input > 1 || input < 0 || isNaN(input)) {
+        throw new Error('input out of range');
+      } else {
+        this.a = (input < 0.5) ? 0 : 1;
+      }
+    } else if (input instanceof String) {
+      this = new BooleanNum(Number(String));
+    } else throw new Error('unsupported type')
+  }
+  add(b) {
+    // 0 + x = x
+    // x + 0 = x
+    // if a and b = 1 then oof
+    if (this.a == 0) return b;
+    else if (b.a == 1) throw new Error('out of range');
+    return this;
+  }
+  sub(b) {
+    if (b.a == 0) return this;
+    else if (this.a == 0) throw new Error('out of range');
+    return new BooleanNum;
+  }
+  mul(b) {
+    if (this.a == 0 || b.a == 0) return new BooleanNum;
+    else return new BooleanNum(1);
+  }
+}
